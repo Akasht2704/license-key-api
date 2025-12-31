51147e6840b40148e36ab8ebd8db6533e31cf645
Akash27045 <utkarsh.tiwari@bytecraftlabs.com>
2025-12-31 17:09:30 +0530

    get_label_data api implemented

---
+++ app/api/get_label_data/route.js
@@ -0,0 +1,56 @@
+import { log } from 'console';
+import {NextResponse} from 'next/server';
+
+
+export async function GET(request) {
+
+    try {
+
+         const { searchParams } = new URL(request.url);
+
+         const a = searchParams.get('a');
+         const b = searchParams.get('b');
+         const c = searchParams.get('c');
+
+         if(a==1 && b==2 && c==3){
+             return NextResponse.json({success: true,
+                  data:[
+                     {
+                         PoductName:'P1NAME',
+                         MRP:'INR 100',
+                         QRCODE:'XYZ1'
+                     },
+                     {
+                         PoductName:'P2NAME',
+                         MRP:'INR 200',
+                         QRCODE:'XYZ2'
+                     },
+                     {
+                         PoductName:'P3NAME',
+                         MRP:'INR 300',
+                         QRCODE:'XYZ3'
+                     },
+                     {
+                         PoductName:'P4NAME',
+                         MRP:'INR 400',
+                         QRCODE:'XYZ4'
+                     },
+                     {
+                         PoductName:'P5NAME',
+                         MRP:'INR 500',
+                         QRCODE:'XYZ5'
+                     },
+                  ] }, { status: 200 });
+            } else {
+                return NextResponse.json({success: false, message:'Invalid Params' }, { status: 400 });
+            }
+        
+    } catch (error) {
+        return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
+        
+    }
+
+   
+
+    
+}
